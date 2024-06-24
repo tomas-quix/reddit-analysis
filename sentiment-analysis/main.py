4615d79b5235f41a03544d341c130880e89ce086
Tomas Neubauer <tomas@quix.ai>
2024-06-24 19:49:39 +0200

    fix

--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 
-app = Application(consumer_group="hugging-face-model-v1.1", auto_offset_reset="earliest")
+app = Application(consumer_group="hugging-face-model-v1.2", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
@@ -20,14 +20,13 @@ classifier = pipeline('sentiment-analysis')
 
 sdf = app.dataframe(input_topic)
 
-
 # Assuming the input data has a 'text' column that you want to process with the model
 sdf['model_result'] = sdf.apply(lambda row: classifier((row["title"] + "\n " + row["selftext"])[:512])[0])
 
 sdf = sdf.update(print)
 
 # Send the processed data to the output topic
-#sdf = sdf.to_topic(output_topic)
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
