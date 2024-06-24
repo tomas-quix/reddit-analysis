f7dc20260e878c04dee45e5c19fa8acc3d973de5
Tomas Neubauer <tomas@quix.ai>
2024-06-24 20:08:55 +0200

    Sentiment average

---
+++ sentiment-average/main.py
@@ -0,0 +1,27 @@
+import os
+from datetime import timedelta
+from quixstreams import Application
+
+# for local dev, load env vars from a .env file
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Application(consumer_group="sentiment-average-v1", auto_offset_reset="earliest")
+
+input_topic = app.topic(os.environ["input"])
+output_topic = app.topic(os.environ["output"])
+
+sdf = app.dataframe(input_topic)
+
+sdf = sdf[sdf["model_result"]["score"] > 0.8]
+
+sdf = sdf.apply(lambda row: 1 if row["model_result"]["label"] == "POSITIVE" else -1)
+
+sdf = sdf.hopping_window(timedelta(hours=1), timedelta(minutes=1), 5000).mean().final()
+
+sdf = sdf.update(lambda row: print(row))
+
+#sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
