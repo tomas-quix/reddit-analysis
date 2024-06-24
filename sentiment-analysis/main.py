b253dca7e27cff53353d0f8832e6ab14c0cfe797
Tomas Neubauer <tomas@quix.ai>
2024-06-24 19:04:13 +0200

    Sentiment analysis fix

--- sentiment-analysis/main.py
+++ sentiment-analysis/main.py
@@ -7,24 +7,27 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
-app = Application(consumer_group="hugging-face-model-v1", auto_offset_reset="earliest")
+
+
+app = Application(consumer_group="hugging-face-model-v1.1", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
 
 # Download the Hugging Face model (list of available models here: https://huggingface.co/models)
 # suggested default is distilbert-base-uncased-finetuned-sst-2-english
-model_name = os.environ["HuggingFaceModel"]
-print("Downloading {0} model...".format(model_name))
-model_pipeline = pipeline(model=model_name)
+classifier = pipeline('sentiment-analysis')
 
 sdf = app.dataframe(input_topic)
 
+
 # Assuming the input data has a 'text' column that you want to process with the model
-sdf['model_result'] = sdf['text'].apply(lambda t: json.dumps(model_pipeline(t)))
+sdf['model_result'] = sdf.apply(lambda row: classifier((row["title"] + "\n " + row["selftext"])[:512])[0])
+
+sdf = sdf.update(print)
 
 # Send the processed data to the output topic
-sdf = sdf.to_topic(output_topic)
+#sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
