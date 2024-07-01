18a481d1f9303102ff943b577da59eec2c07276a
Tomas Neubauer <tomas@quix.ai>
2024-07-01 17:40:46 +0200

    fixes

--- party-classifier/main.py
+++ party-classifier/main.py
@@ -11,7 +11,7 @@ classifier = pipeline("zero-shot-classification")
 candidate_labels = os.environ["labels"].split(",")
 threshold = float(os.environ["threshold"])
 
-app = Application(consumer_group="party-classifier-v1.2", auto_offset_reset="earliest")
+app = Application(consumer_group="party-classifier-v1.3", auto_offset_reset="earliest")
 
 input_topic = app.topic(os.environ["input"])
 output_topic = app.topic(os.environ["output"])
@@ -28,12 +28,11 @@ sdf = app.dataframe(input_topic)
 sdf["text"] = sdf.apply(lambda row: (row["title"] + "\n " + row["selftext"])[:512])
 sdf["classification"] = sdf.apply(lambda row: classifier(row["text"], candidate_labels))
 
-sdf = sdf.update(print)
 
 sdf["party"] = sdf["classification"].apply(select_party)
 sdf = sdf.update(lambda row: print(f"{row['party']}: {row['text']}"))
 
-#sdf = sdf.to_topic(output_topic)
+sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
