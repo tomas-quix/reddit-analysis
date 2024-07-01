6264f1d40c0abf510e48d02f47205683d4a1cb7b
Tomas Neubauer <tomas@quix.ai>
2024-07-01 10:42:28 +0200

    Dash fixes

--- dashboard/main.py
+++ dashboard/main.py
@@ -2,7 +2,6 @@ import streamlit as st
 from influxdb_client_3 import Point, InfluxDBClient3
 import pandas as pd
 import plotly.express as px
-import datetime
 import os
 from streamlit_autorefresh import st_autorefresh
 
@@ -22,65 +21,68 @@ client = InfluxDBClient3(token=os.environ["INFLUXDB_TOKEN"],
                          org=os.environ["INFLUXDB_ORG"],
                          database=os.environ["INFLUXDB_DATABASE"])
 
+# Streamlit app layout
+st.title('Realtime US election Reddit analysis')
+
+# Add a radio button to select the time period
+time_period = st.radio(
+    "Select time period",
+    ("Last 24 hours", "Last 1 week")
+)
+
+# Set the query based on the selected time period
+if time_period == "Last 24 hours":
+    query_time_interval = "24 hours"
+else:
+    query_time_interval = "7 days"
+
 # Query InfluxDB
-query = '''
+query = f'''
 SELECT *
 FROM "metrics-1hour-average"
-WHERE time > now() - interval '74 hours'
+WHERE time > now() - interval '{query_time_interval}'
 ORDER BY time
 '''
 
-df =  client.query(query=query,
-                   mode="pandas",
-                   language="sql")
+df = client.query(query=query, mode="pandas", language="sql")
 
-
-# # Plot the data using Plotly
+# Plot the data using Plotly
 fig = px.line(
     df, 
     x='time',
     y='average_1h', 
     color='metric',
-    title='Analysis of Reddit regarding US election for last 24 hours',
+    title=f'Analysis of Reddit regarding US election for last {query_time_interval}',
     color_discrete_map={
-                  'Trump': 'red',
-                  'Biden': 'blue',
-                  'Trump_winning': '#FF9999',  # Light red
-                  'Biden_winning': '#9999FF'   # Light blue
-              })
-
-
-
-# Streamlit app layout
-st.title('Realtime US election Reddit analysis')
-st.write('This dashboard visualizes the sentiment average over the last 24 hours displaying 24 hours rolling average updated every minute.')
+        'Trump': 'red',
+        'Biden': 'blue',
+        'Trump_winning': '#FF9999',  # Light red
+        'Biden_winning': '#9999FF'   # Light blue
+    }
+)
 
 st.plotly_chart(fig)
 
-
-words_count_query = '''
+# Update the words count query based on the selected time period
+words_count_query = f'''
 SELECT max(count) as "max", word, party
 FROM "party-words-count"
-WHERE time > now() - interval '24 hours' AND party != 'N/A'
+WHERE time > now() - interval '{query_time_interval}' AND party != 'N/A'
 GROUP BY word, party
 '''
 
-words_df =  client.query(query=words_count_query,
-                   mode="pandas",
-                   language="sql")
+words_df = client.query(query=words_count_query, mode="pandas", language="sql")
 
 def print_pie(df, party: str, column):
     df = df[df["party"] == party]
 
     aggregated_df = df.groupby('word')['max'].max().reset_index()
-
     sorted_df = aggregated_df.sort_values(by='max', ascending=False)
 
-
-    fig = px.pie(sorted_df[:20], values='max', names='word', title='Most used words in last 24 hours for ' + party)
+    fig = px.pie(sorted_df[:20], values='max', names='word', title=f'Most used words in last {query_time_interval} for ' + party)
     column.plotly_chart(fig)
-    
-    # Create two columns for the pie charts
+
+# Create two columns for the pie charts
 col1, col2 = st.columns(2)
 
 with col1:
@@ -88,4 +90,3 @@ with col1:
 
 with col2:
     print_pie(words_df, "Conservative", col2)
-    
